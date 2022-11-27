1aab29d16379e846f20d9be0f8e391ff91b32346
DELL <DELL@satya>
2022-11-27 21:12:23 +0530

    zcxcsac

---
+++ src/main/java/Exp_10/Exp_10/App.java
@@ -0,0 +1,13 @@
+package Exp_10.Exp_10;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
